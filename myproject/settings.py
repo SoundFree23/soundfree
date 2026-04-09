ee763a455a8fe67f6419841739acdf7c4d0d7c86
SoundFree23 <soundfree23@users.noreply.github.com>
2026-04-09 17:53:44 +0300

    Add Oblio API integration for automatic proforma invoices

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,7 +124,7 @@ LOGIN_URL = '/login/'
 OBLIO_EMAIL = os.environ.get('OBLIO_EMAIL', 'oprearaz@yahoo.com')
 OBLIO_SECRET = os.environ.get('OBLIO_SECRET', 'e1843087eaba977ef8dcde2203054b6c2a3d3e16')
 OBLIO_CIF = '54416770'
-OBLIO_SERIES = os.environ.get('OBLIO_SERIES', 'Concept23')
+OBLIO_SERIES = os.environ.get('OBLIO_SERIES', 'PRO')
 
 # Logging
 LOGGING = {
